1d282436f47ec88e429b0aa345fa77c700850b7c
therealsarahp <poultonse@gmail.com>
2021-03-22 18:50:31 -0500

    updates have been made

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,28 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max=500, message="Please Provide A Detailed Description")
+    private String description;
+
+    public Skill(String description){
+//        super(name);
+        this.description = description;
+
+    }
+
+    public Skill(){};
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
