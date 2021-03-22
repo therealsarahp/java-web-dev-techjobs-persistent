8c260898f1ba8f6122f31042d1f0d14e4b81ce83
therealsarahp <poultonse@gmail.com>
2021-03-22 15:24:15 -0500

    queries added

--- queries.sql
+++ queries.sql
@@ -1,6 +1,12 @@
 ## Part 1: Test it with SQL
 
-## Part 2: Test it with SQL
+Select column_name, data_type from information_schema.columns
+where table_schema = 'techjobs' AND table_name = 'job';
+
+## Part 2: Test it with SQL(THIS MAY NEED WORK)
+
+SELECT name from techjobs.employer
+where location = "%louis%";
 
 ## Part 3: Test it with SQL
 
