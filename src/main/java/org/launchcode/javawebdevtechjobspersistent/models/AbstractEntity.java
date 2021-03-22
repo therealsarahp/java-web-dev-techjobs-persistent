b29f24a37d39c29d2527dd79769ebb17507e06a8
therealsarahp <poultonse@gmail.com>
2021-03-22 18:49:11 -0500

    updates have been made

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
@@ -1,13 +1,28 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotBlank(message="Name Is Required.")
+    @Size(min=3, max=250)
     private String name;
+
+//    public AbstractEntity(String name) {
+//        this.name=name;
+//    }
+
+    public AbstractEntity(){};
 
     public int getId() {
         return id;
